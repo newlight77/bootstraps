3ffabdfd9fce62e0d20d26cba1c6db7ec0fc6b4a
Kong To <newlight77@gmail.com>
2020-04-24 13:28:31 +0200

    adding bootstrap for spring-kotlin

---
+++ bootstrap-spring-kotlin/src/main/kotlin/com/newlight77/bootstrap/BootstrapSpringKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.newlight77.bootstrap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BootstrapSpringKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<BootstrapSpringKotlinApplication>(*args)
+}
