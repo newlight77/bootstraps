2d06c4cf7a94eb7c6d0d0069c6de964ce5d1dc9c
Kong To <newlight77@gmail.com>
2020-04-24 13:28:17 +0200

    adding bootstrap for spring-java

---
+++ bootstrap-spring-java/src/main/java/com/newlight77/bootstrap/BootstrapSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.newlight77.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapSpringJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstrapSpringJavaApplication.class, args);
+	}
+
+}
---
+++ bootstrap-spring-java/src/test/java/com/newlight77/bootstrap/BootstrapSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newlight77.bootstrap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapSpringJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
