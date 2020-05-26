1832190b4ffa657ebab82b9c0e866610f737b01e
Kong To <newlight77@gmail.com>
2020-05-26 10:09:57 +0200

    added bootstrap-maven-junit5

---
+++ bootstrap-maven-junit5/src/test/java/io/github/newlight77/bootstrap/AppMainTest.java
@@ -0,0 +1,14 @@
+package io.github.newlight77.bootstrap;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppMainTest {
+
+    @Test
+    void myTest() {
+        assertEquals(true, true);
+    }
+
+}
