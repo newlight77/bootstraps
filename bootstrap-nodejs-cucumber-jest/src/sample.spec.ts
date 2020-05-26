17d9cf1cb726fcce9255360284fb6a6c29d2b338
Kong To <newlight77@gmail.com>
2020-05-26 10:04:50 +0200

    added bootstrap-nodejs-cucumber-jest

---
+++ bootstrap-nodejs-cucumber-jest/src/sample.spec.ts
@@ -0,0 +1,13 @@
+import { Sample } from './sample'
+
+describe('Playing ', () => {
+  it('should return xxx when yyy', () => {
+    const input = ''
+    const output = 0
+
+    const result = Sample(input)
+
+    expect(result).toEqual(output)
+  })
+
+})
