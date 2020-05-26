fa51b2aed8ad2a3c4f8a013b783f651dec6774ca
Kong To <newlight77@gmail.com>
2020-05-26 10:16:42 +0200

    added bootstrap-maven-cucumber-junit4

---
+++ bootstrap-maven-cucumber-junit4/src/main/java/io/github/newlight77/kata/MainApp.java
@@ -0,0 +1,15 @@
+package io.github.newlight77.kata;
+
+import java.io.File;
+import java.io.FilenameFilter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+
+}
