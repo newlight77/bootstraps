b37441f92b51b71e7fe5d7bd3dcc492c4be74654
Kong To <newlight77@gmail.com>
2020-05-28 06:34:48 +0200

    some minor corrections

--- bootstrap-maven-spring-cucumber-junit5/src/test/java/io/github/newlight77/kata/ApplicationTests.java
+++ bootstrap-maven-spring-cucumber-junit5/src/test/java/io/github/newlight77/kata/ApplicationTests.java
@@ -1,13 +1,13 @@
 package io.github.newlight77.kata;
 
-//import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
-//
-// @SpringBootTest
-// class BowlingApplicationTests {
-//
-// 	@Test
-// 	void contextLoads() {
-// 	}
-//
-// }
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
